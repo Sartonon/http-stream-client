c9ddd5ca53b81febd00cb84bf48750ae25476c72
Santeri Rusila <santtu_1993@hotmail.com>
2018-05-22 11:30:23 +0300

    prettify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import http from 'stream-http';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import http from "stream-http";
+import axios from "axios";
+import "./App.css";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -13,7 +13,7 @@ class App extends Component {
     username: "",
     usernameConfirmed: false,
     message: "",
-    color: 0,
+    color: 0
   };
 
   componentDidMount() {
@@ -42,13 +42,15 @@ class App extends Component {
   }
 
   getPastMessages = async () => {
-    const { data } = await axios.get("http://httpstream.sartonon.fi/api/pastMessages");
+    const { data } = await axios.get(
+      "http://httpstream.sartonon.fi/api/pastMessages"
+    );
     this.setState({ messages: data });
   };
 
   handleCommand = data => {
     if (data.message[0] === "#") {
-      const splittedMessage = data.message.split('::');
+      const splittedMessage = data.message.split("::");
       const command = splittedMessage[0];
       if (command === "#open") {
         window.open(splittedMessage[1], "_self");
@@ -70,41 +72,44 @@ class App extends Component {
         }, interval || 1000);
       }
     }
-  }
+  };
 
   getMessages = async () => {
     try {
-      this.messagesRequest = http.get('http://httpstream.sartonon.fi/api/messages', res => {
-        console.log("Connection Opened");
-        res.on('data', buf => {
-          if (buf.toString() !== "Connection open") {
-            try {
-              const message = JSON.parse(buf.toString())
-              this.handleMessage(message);
-            } catch (err) {
-              console.log(err);
+      this.messagesRequest = http.get(
+        "http://httpstream.sartonon.fi/api/messages",
+        res => {
+          console.log("Connection Opened");
+          res.on("data", buf => {
+            if (buf.toString() !== "Connection open") {
+              try {
+                const message = JSON.parse(buf.toString());
+                this.handleMessage(message);
+              } catch (err) {
+                console.log(err);
+              }
             }
-          }
-        });
-      
-        res.on('end', () => {
-          console.log("End connection");
-          try {
-            this.getMessages();
-          } catch (err) {
-            console.log("Reconnecting failed: ", err);
-          }
-        });
+          });
 
-        res.on('error', () => {
-          console.log("Connection error");
-          try {
-            this.getMessages();
-          } catch (err) {
-            console.log("Reconnecting failed: ", err);
-          }
-        });
-      })
+          res.on("end", () => {
+            console.log("End connection");
+            try {
+              this.getMessages();
+            } catch (err) {
+              console.log("Reconnecting failed: ", err);
+            }
+          });
+
+          res.on("error", () => {
+            console.log("Connection error");
+            try {
+              this.getMessages();
+            } catch (err) {
+              console.log("Reconnecting failed: ", err);
+            }
+          });
+        }
+      );
     } catch (err) {
       console.log("error in request: ", err);
       this.getMessages();
@@ -112,12 +117,12 @@ class App extends Component {
     console.log("tultiin ulos");
   };
 
-  sendMessage = (e) => {
+  sendMessage = e => {
     e.preventDefault();
     axios.post("http://httpstream.sartonon.fi/api/messages", {
       name: this.state.username,
       message: this.state.message,
-      color: this.state.color,
+      color: this.state.color
     });
     this.setState({ message: "" });
   };
@@ -125,7 +130,7 @@ class App extends Component {
   handleMessage = data => {
     console.log(data);
     this.handleCommand(data);
-    this.setState({ messages: [ ...this.state.messages, data ] });
+    this.setState({ messages: [...this.state.messages, data] });
     setTimeout(() => {
       const objDiv = document.getElementById("chatwindow");
       if (objDiv) {
@@ -134,19 +139,19 @@ class App extends Component {
     }, 200);
   };
 
-  changeUsername = (e) => {
-    this.setState({ username: e.target.value })
+  changeUsername = e => {
+    this.setState({ username: e.target.value });
   };
 
   confirmUsername = () => {
     this.setState({
       usernameConfirmed: true,
-      color: `green`,
+      color: `green`
     });
     // this.getPastMessages();
   };
 
-  handleMessageChange = (e) => {
+  handleMessageChange = e => {
     this.setState({ message: e.target.value });
   };
 
@@ -154,9 +159,9 @@ class App extends Component {
     if (this.messageInterval) clearInterval(this.messageInterval);
     this.messageInterval = setInterval(() => {
       axios.post("http://httpstream.sartonon.fi/api/messages", {
-        name: 'Santeri',
-        message: 'Moikka!',
-        color: 'green',
+        name: "Santeri",
+        message: "Moikka!",
+        color: "green"
       });
     }, this.state.interval || 1000);
   };
@@ -165,8 +170,16 @@ class App extends Component {
     return this.state.messages.map((message, i) => {
       return (
         <div className="Message-wrapper" key={i}>
-          <div className="Message-block" key={i} style={{ float: message.name === this.state.username ? "right" : "left" }}>
-            <div className="Message-name" style={{ color: message.color }}>{message.name}</div>
+          <div
+            className="Message-block"
+            key={i}
+            style={{
+              float: message.name === this.state.username ? "right" : "left"
+            }}
+          >
+            <div className="Message-name" style={{ color: message.color }}>
+              {message.name}
+            </div>
             <div className="Message-content">{message.message}</div>
           </div>
         </div>
@@ -184,24 +197,41 @@ class App extends Component {
           <button onClick={this.startSending}>Laheta</button>
           <input onChange={e => this.setState({ interval: e.target.value })} />
         </header>
-        {!usernameConfirmed ?
+        {!usernameConfirmed ? (
           <div className="Login-div">
-            <p style={{ fontWeight: "bold", fontSize: "16px" }}>Anna käyttäjänimi</p>
-            <input className="Login-input" value={this.state.username} onChange={this.changeUsername} type="text" />
-            <div className="Ok-button" onClick={this.confirmUsername}>Ok</div>
-          </div> :
+            <p style={{ fontWeight: "bold", fontSize: "16px" }}>
+              Anna käyttäjänimi
+            </p>
+            <input
+              className="Login-input"
+              value={this.state.username}
+              onChange={this.changeUsername}
+              type="text"
+            />
+            <div className="Ok-button" onClick={this.confirmUsername}>
+              Ok
+            </div>
+          </div>
+        ) : (
           <div className="Chat-window">
             <div id="chatwindow" className="Message-div">
               {this.renderMessages()}
             </div>
             <form onSubmit={this.sendMessage}>
               <div className="Chat-input">
-                <input className="Chat-inputfield" onChange={this.handleMessageChange} value={this.state.message} type="text" />
-                <div className="Send-button" onClick={this.sendMessage}>Lähetä</div>
+                <input
+                  className="Chat-inputfield"
+                  onChange={this.handleMessageChange}
+                  value={this.state.message}
+                  type="text"
+                />
+                <div className="Send-button" onClick={this.sendMessage}>
+                  Lähetä
+                </div>
               </div>
             </form>
           </div>
-        }
+        )}
       </div>
     );
   }
